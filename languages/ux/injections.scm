f12d1062f153a3bf1b5a6b587d357c94a462c665
xuxu <null>
2026-07-24 18:22:55 +0800

    高亮代码段

---
+++ languages/ux/injections.scm
@@ -0,0 +1,102 @@
+; <script>
+((script_element
+  (start_tag) @_no_lang
+  (raw_text) @content)
+  (#not-match? @_no_lang "lang=")
+  (#set! "language" "javascript"))
+
+; <script lang="js">
+((script_element
+  (start_tag
+    (attribute
+      (attribute_name) @_lang
+      (quoted_attribute_value
+        (attribute_value) @_js)))
+  (raw_text) @content)
+  (#eq? @_lang "lang")
+  (#eq? @_js "js")
+  (#set! "language" "javascript"))
+
+; <script lang="ts">
+((script_element
+  (start_tag
+    (attribute
+      (attribute_name) @_lang
+      (quoted_attribute_value
+        (attribute_value) @_ts)))
+  (raw_text) @content)
+  (#eq? @_lang "lang")
+  (#eq? @_ts "ts")
+  (#set! "language" "typescript"))
+
+; <script lang="tsx">
+; <script lang="jsx">
+; Zed built-in tsx, we mark it as tsx ^:)
+(script_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr
+      (quoted_attribute_value
+        (attribute_value) @language)))
+  (#eq? @_attr "lang")
+  (#any-of? @language "tsx" "jsx")
+  (raw_text) @content)
+
+; {{ }}
+((interpolation
+  (raw_text) @content)
+  (#set! "language" "typescript"))
+
+; v-
+(directive_attribute
+  (quoted_attribute_value
+    (attribute_value) @content
+    (#set! "language" "typescript")))
+
+; Vue <style lang="css"> injections
+(style_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr_name
+      (#eq? @_attr_name "lang")
+      (quoted_attribute_value
+        (attribute_value) @language)))
+  (raw_text) @content)
+
+; Vue <style lang="scss"> injections
+((style_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr_name
+      (#eq? @_attr_name "lang")
+      (quoted_attribute_value
+        (attribute_value) @language)))
+  (raw_text) @content)
+  (#eq? @language "scss")
+  (#set! "language" "scss"))
+
+; Vue <style> css injections (no lang attribute)
+(style_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr_name)*)
+  (raw_text) @content
+  (#not-any-of? @_attr_name "lang")
+  (#set! language "css"))
+
+; <template lang="pug">
+((template_element
+  (start_tag
+    (attribute
+      (attribute_name) @_lang
+      (quoted_attribute_value
+        (attribute_value) @_pug)))
+  (text) @content)
+  (#eq? @_lang "lang")
+  (#eq? @_pug "pug")
+  (#set! language "pug"))
+
+; <!-- -->
+; Make comment as html
+((comment) @content
+  (#set! language "html"))
